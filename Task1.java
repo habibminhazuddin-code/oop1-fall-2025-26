a145db7bd833be9e33f56eab03ff9f50b9c1bff7
habibminhazuddin-code <habibminhazuddin@gmail.com>
2025-10-28 17:31:43 +0600

    25-61944-2

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1{
+public static void main(String[]args){
+    System.out.println("The product price :250");
+     int x=250;
+     float y=x*0.15f;
+     System.out.print("After discount:");
+     System.out.println(y);
+
+}
+} 
+    
+
---
+++ Task4.java
@@ -0,0 +1,7 @@
+public class Task4 {
+    public static void main(String []args){
+        float c=32f;
+        float f=(1.8f*c)+32;
+        System.out.println("In celsius: "+c+ " In fahrenhiet:" +f);
+    }
+}
