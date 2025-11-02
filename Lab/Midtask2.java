eac3ed05a064c5ca9cdb996e735fde3d626d052b
habibminhazuddin-code <habibminhazuddin@gmail.com>
2025-11-02 17:09:20 +0600

    25-61944-2

---
+++ Lab/Midtask2.java
@@ -0,0 +1,47 @@
+public class Midtask2 {
+    public static void main(String[]args){
+        String str1 = "Habib";
+        String str2 = "Uddin";
+        String FullName = str1+ "" +str2;
+        System.out.println("Your full name is :" + FullName);
+
+        String productName ="Call of duty";
+        String productId = "chikito 13";
+        String procode = productName + "" + productId;
+        System.out.println("Your Product code is: " + procode);
+
+        String email ="ezioauditore32@gmail.com";
+        String substring = email.substring(0, 24);
+
+        System.out.println ("substring : " + substring);
+
+        String uppername ="Minhaz uddin ";
+        String uppergen = uppername.toUpperCase();
+        String lowergen = uppername.toLowerCase();
+        System.out.println("Uppercase:"+ uppergen);
+        System.out.println("lowecase:"+ lowergen);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }       
+}
---
+++ Lab/Task2.java
@@ -0,0 +1,19 @@
+public class Task2{
+
+public static void main (String[]args){
+   int s1=65;
+   int s2=76;
+   int s3=90;
+   System.out.print("Total Number of three student:");
+   int s4 =s1+s2+s3;
+   System.out.print(s4);
+   System.out.print("Now the avarage:");
+   float s5=s4/3f;
+   System.out.print(s5);
+
+}
+}
+
+
+    
+
