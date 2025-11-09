6e16245c13a63dc9a856eb600b34bf82dd416b62
habibminhazuddin-code <habibminhazuddin@gmail.com>
2025-11-09 22:40:05 +0600

    25-61944-2

--- Project/Mortgagecalculator1.java
+++ Project/Mortgagecalculator1.java
@@ -1,4 +1,4 @@
-   
+
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
@@ -7,53 +7,100 @@ public class Mortgagecalculator1 {
     public static void main(String[] args) {
         System.out.println("=== Bangladesh Mortgage Calculator (Dollar) ===");
         Scanner sc = new Scanner(System.in);
+        double salary = 0;
+        int creditScore = 0;
+        boolean criminalRecord = false;
+        int num ;
+        float num2 = 0;
+        int num3 =0;
 
-        
-
-                if (sc.hasNextDouble()) {
-    double num = sc.nextDouble();
-    System.out.println(" You entered: " + num);
-} else {
-    System.out.println(" Invalid input! Please enter numbers only.");
-}
-            
-
-            
-
-        System.out.print("Enter your salary: ");
-        double salary = sc.nextDouble();
+        while(true){
+              System.out.print("Enter your income: ");
+        if (sc.hasNextDouble())  {      
+                salary = sc.nextDouble();
+               break;
+        }else {
+            System.out.println("Invalid input.Please enter a numeric value");
+            sc.next();
+        }
+    }
+       
+        while(true){
         System.out.print("Enter Your credit score (0-500) :");
-        int creditScore = sc.nextInt();
-        System.out.print("do you have any criminal record (True/False) :");
-        boolean criminalRecord = sc.nextBoolean();
-
-        if (creditScore < 0 || creditScore > 500) {
-                System.out.println("Credit Score must be between 0 and 500.");
-                
+        if (sc.hasNextDouble())  {
+                creditScore = sc.nextInt();
+        if (creditScore >=0 || creditScore < 500) {
+            break;
+        } else{
+             System.out.println("Credit Score must be between 0 and 500.");    
             }
 
+        } else {
+            System.out.println("Invalid input.Please enter numeric value between 0 and 500");
+            sc.next();
+
+        } 
+    }
+        
+            while(true){  
+                    System.out.print("Do you have any criminal record (True/False) :");
+             if(sc.hasNextBoolean()) {
+                     criminalRecord = sc.nextBoolean();
+                    break;
+             } else{
+                System.out.println("Invalid input.please type true or false");
+                sc.next();
+             }
+
+            }
             // Loan Eligibility Check
-            if (salary <= 0 || creditScore < 300 || criminalRecord) {
+            boolean eligible = (creditScore >=300) && !criminalRecord;
+            
+            if (!eligible) {
                 System.out.println(" You are not eligible for a loan.");
                 System.out.println("Reason:");
-                if (salary <= 0) System.out.println("- Salary must be greater than 0.");
-                if (creditScore < 300) System.out.println("- Credit score must be at least 300.");
-                if (criminalRecord) System.out.println("- Applicant must not have a criminal record.");}
-                
-        System.out.print("Enter loan amount (principal in BDT): ");
-        int num = sc.nextInt();
-        if(num > 2* salary){
-            System.out.println("loan amount cannot exceed your twice salary");
+                System.out.println("- Salary must be greater than 0.");
+                System.out.println("- Credit score must be at least 300.");
+                System.out.println("- Applicant must not have a criminal record.");
+                sc.close();
+                return;
+            }
+        
+          while (true) {     
+               System.out.print("Enter loan amount (principal in BDT): ");
+          if (sc.hasNextInt()) {
+                 num = sc.nextInt(); 
+        if(num <= 2* salary){
+            break;
+        } else {
+            System.out.println("Loan request denied .loan amount cannot exceed your twice salary");
         }
+    }
+}
+       while(true) {
+             System.out.print("Enter annual interest rate: ");
+             if( sc.hasNextFloat()){
+                     num2 = sc.nextFloat();
+             break;
+             } else{
+                System.out.println("Invalid input.Please enter a numeric value");
+                sc.next();
+             }
+             }
  
-        System.out.print("Enter annual interest rate: ");
-        float num2 = sc.nextFloat();
+         while(true) {
+               System.out.print("Enter loan period (in years): ");
+             if(sc.hasNextInt()){
+                     num3 = sc.nextInt();
+                    break;
+             } else {
+                System.out.println("Invalid input .Please enter numeric value");
+                sc.next();
+             }
+             }
  
-        System.out.print("Enter loan period (in years): ");
-        int num3 = sc.nextInt();
- 
-        float r = (num2 / 100 / 12);  
-        int n = num3 * 12;          
+        float r = ( num2 / 100f / 12f);  
+        int n = num3* 12;          
  
         double m = (double) num * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
         double totalPayment = m * n;
@@ -62,8 +109,6 @@ public class Mortgagecalculator1 {
          
          
         System.out.println("=== Mortgage Summary ===");
- 
-       
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(bdLocale);
         NumberFormat percentFormatter = NumberFormat.getInstance();
  
@@ -81,7 +126,10 @@ public class Mortgagecalculator1 {
         sc.close();
     }  
 
-}
+    }
+
+
+
 
  
 
